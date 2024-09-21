9358897bb56fab3e70d65eeeb02af28f0c772863
Eduardo <eduardoshw123@gmail.com>
2024-09-21 13:39:51 -0300

    adicionei a função 'informações_de_equipe()' e pequenos ajustes na função 'menu_principal()'

--- main.c
+++ main.c
@@ -9,6 +9,7 @@ void informacoes_da_equipe(void);
 int main(void){
     menu_principal();
     informacoes_gerais();
+    informacoes_da_equipe();
 
     printf("fim");
 
@@ -24,14 +25,15 @@ void menu_principal(void){
     printf("||                                                                ||\n");    
     printf("| = = = = = Sistema de Gestão para uma loja de conveniencia  = = = |\n");    
     printf("||                                                                ||\n");    
-    printf("||  1 - Modulo de produtos                                        ||\n");    
-    printf("||  2 - Modulo de clientes                                        ||\n");    
-    printf("||  3 - Modulo de vendas                                          ||\n");    
-    printf("||  4 - Modulo de relatorios                                      ||\n");    
-    printf("||  5 - Modulo de informações                                     ||\n");    
-    printf("||  0 - sair                                                      ||\n");    
+    printf("||    1 - Modulo de produtos                                      ||\n");    
+    printf("||    2 - Modulo de clientes                                      ||\n");    
+    printf("||    3 - Modulo de vendas                                        ||\n");    
+    printf("||    4 - Modulo de relatorios                                    ||\n");    
+    printf("||    5 - Modulo de informações                                   ||\n");    
+    printf("||    6 - Modulo de informações da equipe                         ||\n");    
+    printf("||    0 - sair                                                    ||\n");    
     printf("||                                                                ||\n");    
-    printf("||  => Escolha a opção desejada: ");
+    printf("||    => Escolha a opção desejada: ");
     scanf("%c", &op);
     getchar();
     printf("||                                                                ||\n");    
@@ -60,6 +62,27 @@ void informacoes_gerais(void){
     printf("||  para uma loja de conveniência, com o intuito de ser avaliado  ||\n");    
     printf("||  na disciplina DCT1106 - Programação.                          ||\n");    
     printf("||                                                                ||\n");    
+    printf("|==================================================================|\n");    
+    printf("\n");
+    printf("| Tecle <ENTER> para continuar...\n");
+    getchar();
+}
+
+void informacoes_da_equipe(void){
+    system("clear||cls");
+    printf("\n");
+    printf("____________________________________________________________________\n");
+    printf("||                                                                ||\n");
+    printf("|| = = = = = = = =   Menu de informações da equipe  = = = = = = = ||\n");
+    printf("||                                                                ||\n");
+    printf("||   Projeto desenvolvido por:                                    ||\n");
+    printf("||                                                                ||\n");
+    printf("||     Eduardo Nascimento Santos                                  ||\n");
+    printf("||     github: https://github.com/Eduardo-NSantos                 ||\n");
+    printf("||                                                                ||\n");
+    printf("||     Luiz Henrique Felix                                        ||\n");
+    printf("||     github: https://github.com/LuizFelixDev                    ||\n");
+    printf("||                                                                ||\n");
     printf("|==================================================================|\n");    
     printf("\n");
     printf("| Tecle <ENTER> para continuar...\n");
